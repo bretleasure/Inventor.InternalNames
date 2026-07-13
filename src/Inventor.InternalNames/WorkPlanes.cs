6ab55cb703b937bde74811f353a257f51a53714c
Bret Leasure <bret.leasure@outlook.com>
2026-07-12 20:05:42 -0500

    Added work features, model states, and view representations

---
+++ src/Inventor.InternalNames/WorkPlanes.cs
@@ -0,0 +1,8 @@
+namespace Inventor.InternalNames;
+
+public struct WorkPlanes
+{
+	public const string XY = "XY Plane";
+	public const string XZ = "XZ Plane";
+	public const string YZ = "YZ Plane";
+}
